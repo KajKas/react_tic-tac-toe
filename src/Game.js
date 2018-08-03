dd6f3173975ae139786bbebaedc969591dd9a3d8
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-08-03 20:04:57 +0200

    putted all in one file and adding history

--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import Board from './index'
+import calculateWinner from './index'
 
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
+// class Game extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       history: [{
+//         squares: Array(9).fill(null)
+//       }],
+//       xIsNext: true
+//     }
+//   }
+//     render() {
+//       const history = this.state.history
+//       const current = history[history.length - 1]
+//       const winner = calculateWinner(current.squares)
+//
+//
+//         return (
+//             <div className="game">
+//                 <div className="game-board">
+//                     <Board />
+//                 </div>
+//                 <div className="game-info">
+//                     <div>{/* status */}</div>
+//                     <ol>{/* TODO */}</ol>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
 
 export default Game
--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './Game';
-import Square from './Square'
-// import Board from './Board'
 import registerServiceWorker from './registerServiceWorker';
 
-
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice()
-    squares[i] = this.state.xIsNext ? 'X' : 'O'
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    })
-  }
 
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
@@ -45,7 +32,6 @@ class Board extends React.Component {
     }
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -65,6 +51,50 @@ class Board extends React.Component {
     );
   }
 }
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      stepsNumber: 0,
+      xIsNext: true
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
+  render() {
+    const history = this.state.history
+    const current = history[history.length - 1]
+    const winner = calculateWinner(current.squares)
+
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+
 
 function calculateWinner(squares) {
   const lines = [
@@ -90,7 +120,7 @@ function calculateWinner(squares) {
 
 registerServiceWorker();
 
-export default Board
+// export default Board
 
 ReactDOM.render(
   <Game />,

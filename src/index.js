5f4004bd45f47d0328526079dbf0fec0f27df7a3
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-08-03 19:40:28 +0200

    works with board component inside index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,69 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Game';
+import Square from './Square'
+// import Board from './Board'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
+
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
 
 function calculateWinner(squares) {
   const lines = [
@@ -33,4 +90,9 @@ function calculateWinner(squares) {
 
 registerServiceWorker();
 
-export default calculateWinner
+export default Board
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
